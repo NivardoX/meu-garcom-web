6b9f50f7f1a0cbc5d5030a35b163fe5832949231
Luishgfarias <luishgfarias@gmail.com>
2023-06-27 14:21:57 -0300

    up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,22 @@
-import { AuthRouter } from "./auth.routes";
-import { useAuth } from "../hooks/useAuth";
-import * as AuthProvider from "../hooks/useAuthProvider";
-import { RestaurantRouter } from "./Restaurant/restaurant.routes";
-import { ProviderRouter } from "./Provider/provider.routes";
-import { useNavigate } from "react-router-dom";
-
+import { AuthRouter } from './auth.routes'
+import { useAuth } from '../hooks/useAuth'
+import * as AuthProvider from '../hooks/useAuthProvider'
+import { RestaurantRouter } from './Restaurant/restaurant.routes'
+import { ProviderRouter } from './Provider/provider.routes'
 export function Router() {
-  const { restaurantSession } = useAuth();
-  const { providerSession } = AuthProvider.useAuth();
+  const { restaurantSession } = useAuth()
+  const { providerSession } = AuthProvider.useAuth()
 
   function redirectByUserType() {
     if (restaurantSession) {
-      return <RestaurantRouter />;
+      return <RestaurantRouter />
     }
     // TODO: Corrigir o contexto de provider
     if (providerSession) {
-      return <ProviderRouter provider={providerSession}/>;
-    } 
-    
-    return <AuthRouter />;
-    
-  }
-  return redirectByUserType();
-}
+      return <ProviderRouter provider={providerSession} />
+    }
 
+    return <AuthRouter />
+  }
+  return redirectByUserType()
+}
